ebd50da696b4544b3ba5d0619139331b80ffbedc
brett weisberg <weisberg.brett@gmail.com>
2018-01-15 20:05:13 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,4 +3,4 @@
    puts " R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}, G: #{rainbow[3]}, B: #{rainbow[4]}, I: #{rainbow[5]}, V: #{rainbow[6]}"
  end
 
-colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+Colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
